20ad721e4b674937a871c27ee621a8e46496acfa
David Held <davheld@eecs.berkeley.edu>
2016-08-14 14:59:13 -0700

    Allowing training with more generated images than batch size

--- src/train/tracker_trainer.cpp
+++ src/train/tracker_trainer.cpp
@@ -59,10 +59,6 @@ void TrackerTrainer::Train(const cv::Mat& image_prev, const cv::Mat& image_curr,
       << " images_batch: " << images_batch_.size() <<
          " bboxes_gt_scaled_batch_: " << bboxes_gt_scaled_batch_.size();
 
-  // We don't currently handle the case in which the number of generated examples per image is larger than the
-  // batch size, so for now, throw an error if kGeneratedExamplesPerImage >= kBatchSize
-  CHECK_LT(kGeneratedExamplesPerImage, kBatchSize);
-
   // Set up example generator.
   example_generator_->Reset(bbox_prev,
                            bbox_curr,
@@ -75,56 +71,50 @@ void TrackerTrainer::Train(const cv::Mat& image_prev, const cv::Mat& image_curr,
   std::vector<BoundingBox> bboxes_gt_scaled;
   MakeTrainingExamples(&images, &targets, &bboxes_gt_scaled);
 
-  // Compute the number of images left to complete the batch.
-  const int num_in_batch = images_batch_.size();
-  const int num_left_in_batch = kBatchSize - num_in_batch;
+  while (images.size() > 0) {
+    // Compute the number of images left to complete the batch.
+    const int num_in_batch = images_batch_.size();
+    const int num_left_in_batch = kBatchSize - num_in_batch;
 
-  // The number of images to use is upper-bounded by the number left in the batch.
-  // The rest go into the next batch.
-  const int num_use = std::min(static_cast<int>(images.size()), num_left_in_batch);
-  const int num_unused = images.size() - num_use;
+    // The number of images to use is upper-bounded by the number left in the batch.
+    // The rest go into the next batch.
+    const int num_use = std::min(static_cast<int>(images.size()), num_left_in_batch);
 
-  if (num_use < 0) {
-    printf("Error: num_use: %d\n", num_use);
-  }
+    if (num_use < 0) {
+      printf("Error: num_use: %d\n", num_use);
+    }
 
-  // Add the approrpriate number of images to the batch.
-  images_batch_.insert(images_batch_.end(),
-                       images.begin(), images.begin() + num_use);
-  targets_batch_.insert(targets_batch_.end(),
-                        targets.begin(), targets.begin() + num_use);
-  bboxes_gt_scaled_batch_.insert(bboxes_gt_scaled_batch_.end(),
-                                 bboxes_gt_scaled.begin(),
-                                 bboxes_gt_scaled.begin() + num_use);
-
-  // If we have a full batch, then train!
-  if (images_batch_.size() == kBatchSize) {    
-    // Increment the batch count.
-    num_batches_++;
-
-    // We have filled up a complete batch, so we should train.
-    ProcessBatch();
-
-    // After training, clear the batch.
-    images_batch_.clear();
-    targets_batch_.clear();
-    bboxes_gt_scaled_batch_.clear();
-
-    // Reserve the appropriate amount of space for the next batch.
-    images_batch_.reserve(kBatchSize);
-    targets_batch_.reserve(kBatchSize);
-    bboxes_gt_scaled_batch_.reserve(kBatchSize);
-  }
-
-  // The remaining generated examples that were not used begin to fill
-  // up the next batch.
-  if (num_unused > 0) {
+    // Add the approrpriate number of images to the batch.
     images_batch_.insert(images_batch_.end(),
-                         images.begin() + num_use, images.end());
+                         images.begin(), images.begin() + num_use);
     targets_batch_.insert(targets_batch_.end(),
-                          targets.begin() + num_use, targets.end());
+                          targets.begin(), targets.begin() + num_use);
     bboxes_gt_scaled_batch_.insert(bboxes_gt_scaled_batch_.end(),
-                                   bboxes_gt_scaled.begin() + num_use,
-                                   bboxes_gt_scaled.end());
+                                   bboxes_gt_scaled.begin(),
+                                   bboxes_gt_scaled.begin() + num_use);
+
+    // If we have a full batch, then train!  Otherwise, save this batch for later.
+    if (images_batch_.size() == kBatchSize) {
+      // Increment the batch count.
+      num_batches_++;
+
+      // We have filled up a complete batch, so we should train.
+      ProcessBatch();
+
+      // After training, clear the batch.
+      images_batch_.clear();
+      targets_batch_.clear();
+      bboxes_gt_scaled_batch_.clear();
+
+      // Reserve the appropriate amount of space for the next batch.
+      images_batch_.reserve(kBatchSize);
+      targets_batch_.reserve(kBatchSize);
+      bboxes_gt_scaled_batch_.reserve(kBatchSize);
+    }
+
+    // Remove the images that were used.
+    images.erase(images.begin(), images.begin() + num_use);
+    targets.erase(targets.begin(), targets.begin() + num_use);
+    bboxes_gt_scaled.erase(bboxes_gt_scaled.begin(), bboxes_gt_scaled.begin() + num_use);
   }
 }
