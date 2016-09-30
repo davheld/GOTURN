882898e7b5454b313599c71084e9cd922b6402a5
blackunicorn15 <kunalsaluja15@gmail.com>
2016-09-29 21:16:45 -0700

    commenting the assignment of Caffe_DIR variable; user must specify the path to his caffe directory

--- cmake/Modules/FindCaffe.cmake
+++ cmake/Modules/FindCaffe.cmake
@@ -1,7 +1,8 @@
 # Caffe package
 unset(Caffe_FOUND)
 
-set(Caffe_DIR /home/kunal/ssd/caffe/distribute)
+###Set the variable Caffe_DIR as the root of your caffe directory
+#set(Caffe_DIR /*Specify path to caffe here*/)
 
 
 find_path(Caffe_INCLUDE_DIRS NAMES caffe/caffe.hpp caffe/common.hpp caffe/net.hpp caffe/proto/caffe.pb.h caffe/util/io.hpp caffe/vision_layers.hpp
